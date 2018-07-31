b52c6de4ab703a355f842cbd566de3a487a18d7f
Andrew Hariri <hariria@usc.edu>
2018-07-31 02:08:06 -0700

    commit

--- V1/1_Scraper_API/ebayAPI.py
+++ V1/1_Scraper_API/ebayAPI.py
@@ -27,7 +27,7 @@ from ebaysdk.shopping import Connection as Shopping
 from ebaysdk.exception import ConnectionError
 
 
-
+centralDictionary = {}
 
 
 # ------ DUMP XML TO A FILE ------- #
@@ -37,6 +37,15 @@ def dumpXML(api, filename="data.xml"):
     print(etree.tostring(x, pretty_print=True, encoding='utf-8'))
     with open(filename, "wb") as f:
         f.write(ET.tostring(tree))
+
+
+# ------ DUMP DICTIONARY TO JSON FILE ------ #
+def dumpDictJSON(dict, filename="data.json"):
+    if os.path.isfile(filename):
+        os.remove(filename)
+
+    with open(filename, 'w') as outfile:
+        json.dump(dict, outfile, sort_keys=True, indent=4)
 
 
 
@@ -50,35 +59,25 @@ def dumpJSON(api, filename="data.json"):
         json.dump(edited, outfile, sort_keys=True, indent=4)
 
 
-
-# ------- SHOPPING ------- #
-def shoppingAPI():
-    api = Shopping(appid='AndrewHa-carmap-PRD-a69dbd521-35d96473', config_file='ebay.yaml',
-                   warnings=True)
-    shoppingDict = ({
-        'ItemID': ['113164620590', '263841289962'],
-        'IncludeSelector': 'ItemSpecifics,TextDescription,Details'
-    })
-    listItemId = findingAPI()
-    # otherList = ['113164620590', '263841289962']
-    shoppingDict['ItemID'] = listItemId[0:19]
-    print(shoppingDict)
-
-
-    try:
-        api.execute('GetMultipleItems', shoppingDict)
-        dumpJSON(api)
-
-    except ConnectionError as e:
-        print(e)
-        print(e.response.dict())
+# ------ APPEND Central DICTIONARY ------ #
+def appendCentral(api, filename="data.json"):
+    if os.path.isfile(filename):
+        with open(filename,'r+') as f:
+            dict = json.load(f)
+            edited = json.loads(api.response.json())
+            dict.update(edited)
+            json.dump(dict, f)
+    else:
+        edited = json.loads(api.response.json())
+        with open(filename, 'w') as outfile:
+            json.dump(edited, outfile, sort_keys=True, indent=4)
 
 
 
+# ------ TOTAL NUMBER OF PAGES ------- #
 def getTotalPages(api, dict=None):
     dictFinding = ({
         'categoryId': '6001',
-        'outputSelector': 'CategoryHistogram',
         'aspectFilter': {
             'aspectName': 'Make',
             'aspectValueName': 'Ferrari'
@@ -134,15 +133,44 @@ def findingAPI():
     except Exception as e:
         print("error % s" % e)
 
-    # dumpJSON(response)
+
+
+
+# ------- SHOPPING ------- #
+def shoppingAPI():
+    api = Shopping(appid='AndrewHa-carmap-PRD-a69dbd521-35d96473', config_file='ebay.yaml',
+                   warnings=True)
+    shoppingDict = ({
+        'ItemID': ['113164620590'],
+        'IncludeSelector': 'ItemSpecifics,TextDescription,Details'
+    })
+    listItemId = findingAPI()
+    for x in range(0, len(listItemId), 20):
+        if (x < len(listItemId)):
+            shoppingDict['ItemID'] = listItemId[x: x + 20]
+        else:
+            maxRange = len(listItemId) - x
+            shoppingDict['ItemID'] = listItemId[x: x + maxRange]
+        try:
+            # IF DICTIONARY IS EMPTY
+            if not centralDictionary:
+                centralDictionary = json.loads('%s' % api.execute('GetMultipleItems', shoppingDict).json())
+            # IF DICTIONARY NOT EMPTY
+            else:
+                response = json.loads('%s' % api.execute('GetMultipleItems', shoppingDict).json())
+                tempList = response['Item']
+                centralDictionary['Item'].append(tempList)
+            appendJSON(api)
+        except ConnectionError as e:
+            print(e)
+            print(e.response.dict())
+    dumpDictJSON(centralDictionary)
+
 
 
 
 
 # ---- MAIN ---- #
 if __name__ == "__main__":
-    # findingAPI()
-    # print("CERTID: ", opts.certid, "\nAPPID: ", opts.appid, "\n")
+
     shoppingAPI()
-    # # run(opts)
-    # tradingAPI(opts)
